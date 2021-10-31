11a919be38cdb679a9cf0beedb1d926e3ce7f7e1
kidpia@hotmail.co.kr <dkrlskfk>
2021-11-01 03:44:13 +0900

    통합본보완중

---
+++ src/main/java/daedan/mes/prod/domain/ProdAttr.java
@@ -0,0 +1,72 @@
+package daedan.mes.prod.domain;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class ProdAttr {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="prodAttrNo",nullable = false, columnDefinition = "numeric")
+    private Long prodAttrNo;
+
+    @Column(name="custNo" , columnDefinition = "numeric")
+    private Long custNo;
+
+    /*품번*/
+    @Column(name="prodNo",nullable = false , columnDefinition = "numeric")
+    private Long prodNo;
+
+    /*제품분류*/
+    @Column(name="prodType",nullable = false , columnDefinition = "numeric")
+    private Long prodType;
+
+    /*연근(4,5,6,기타)*/
+    @Column(name="howOld" , columnDefinition = "numeric")
+    private Long howOld;
+
+    /*진세노(사이드)*/
+    @Column(name="jinseno" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float jinseno;
+
+    // 당도 : 최저당도
+    @Column(name = "minBrix" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float minBrix;
+
+    // 당도 : 최대당도
+    @Column(name = "maxBrix" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float maxBrix;
+
+    // 고형분 :
+    @Column(name = "solid" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float solid;
+
+    // 타입(정타입:점도가 높아서 병제품으로 사용  ,원료타입: 벌크원료로 고객사 또는 후공저의 원료로…)
+    @Column(name = "usedTp" , columnDefinition = "numeric default 0")
+    private Long usedTp;
+
+    // 최소점도 :  (단위:CP : 일반적으로 10000 ~ 35000까지이며 사용자 입력을 처리)
+    @Column(name = "minVisco" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float minVisco;
+
+    // 최대점도 :  (단위:CP : 일반적으로 10000 ~ 35000까지이며 사용자 입력을 처리)
+    @Column(name = "maxVisco" , columnDefinition = "numeric default 0", precision=6, scale=1)
+    private Float maxVisco;
+
+    // 색도 : (단위:ABS: 1…9까지로 표현하며 소숫점 1자리까지)
+    @Column(name = "chroma" , columnDefinition = "numeric default 0", precision=3, scale=1)
+    private Float chroma;
+
+    /*사용여부*/
+    @Column(name="used_yn",nullable = false, length = 1 , columnDefinition = "char default 'Y'")
+    private String usedYn;
+
+
+}
---
+++ src/main/java/daedan/mes/prod/domain/ProdFormat.java
@@ -0,0 +1,20 @@
+package daedan.mes.prod.domain;
+
+public class ProdFormat {
+    public  static final int PROD_NO = 0; //제품번호
+    public static final int PROD_NM = 1; //제품명
+    public static final int ERP_PROD_NM = 2;  //품목제조보고서 제품명
+    public static final int PROD_CODE = 4;  //제품코드
+    public static final int BRNCH_NO = 6; //제조공정
+    public static final int HEAT_TP = 7; //살균기기준명
+
+    public static final int MESS = 8; //단위용량
+    public static final int SAVE_TMPR = 9;  //보관온도
+    public static final int SALE_UNIT = 10;  //판매단위
+    public static final int MNGR_BASE = 11; //질량_중량구분코드
+    public static final int VALID_TERM = 12;  //유통기한
+    public static final int QTY_PER_PKG = 13;  //포장수량
+    public static final int SPGA = 14;  //비율
+    public static final int PROD_SHAPE = 15;  //제품형태
+    public static final int BOM_LVL = 16;  //제품형태
+}
