40c2fbb10930a61aacc717a070439e643947ef1c
jun <ghgh9795@naver.com>
2021-11-25 11:20:39 +0900

    표준화 작업

--- src/main/java/daedan/mes/kpi/controller/KpiController.java
+++ src/main/java/daedan/mes/kpi/controller/KpiController.java
@@ -3,7 +3,10 @@ package daedan.mes.kpi.controller;
 import daedan.mes.common.domain.Result;
 import daedan.mes.dash.service.DashService;
 import daedan.mes.kpi.service.KpiService;
+import daedan.mes.user.domain.AccHstr;
+import daedan.mes.user.domain.EvntType;
 import daedan.mes.user.domain.UserInfo;
+import daedan.mes.user.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,28 +24,54 @@ import java.util.Map;
 public class KpiController {
     private Log log = LogFactory.getLog(this.getClass());
 
-
     @Autowired
     private KpiService kpiService;
 
+    @Autowired
+    private UserService userService;
 
     @PostMapping(value="/operPerformRead")
     public Result operPerformRead(@RequestBody Map<String, Object> paraMap  , HttpSession session){
         Result result = Result.successInstance();
-
         UserInfo uvo = (UserInfo) session.getAttribute("userInfo");
+        Long custNo = uvo.getCustInfo().getCustNo();
+
         paraMap.put("custNo", uvo.getCustInfo().getCustNo());
         paraMap.put("kpiSessId",session.getAttribute("kpiSessId"));
         result.setData(kpiService.operPerformRead(paraMap));
+
+        //SOL AddOn By KMJ AT 21.11.16
+        if (uvo.getCustInfo().getActEvtLogYn().equals("Y")) {
+            try {
+                AccHstr acvo = (AccHstr) session.getAttribute("acchstr");
+                userService.saveAccLogEvnt(custNo, acvo.getAccNo(), EvntType.READ, 1);
+            } catch (NullPointerException ne) {
+            }
+        }
+        //EOL AddON By KMJ AT 21.11.26
+
         return result;
     }
     @PostMapping(value="/kpiRead")
     public Result kpiRead(@RequestBody Map<String, Object> paraMap  , HttpSession session){
         Result result = Result.successInstance();
         UserInfo uvo = (UserInfo) session.getAttribute("userInfo");
+        Long custNo = uvo.getCustInfo().getCustNo();
+
         paraMap.put("custNo", uvo.getCustInfo().getCustNo());
         paraMap.put("kpiSessId",session.getAttribute("kpiSessId"));
         result.setData(kpiService.kpiRead(paraMap));
+
+        //SOL AddOn By KMJ AT 21.11.16
+        if (uvo.getCustInfo().getActEvtLogYn().equals("Y")) {
+            try {
+                AccHstr acvo = (AccHstr) session.getAttribute("acchstr");
+                userService.saveAccLogEvnt(custNo, acvo.getAccNo(), EvntType.READ, 1);
+            } catch (NullPointerException ne) {
+            }
+        }
+        //EOL AddON By KMJ AT 21.11.26
+
         return result;
     }
 
