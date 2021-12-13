c8d036e6ede0027bbfed98f9d5519045d1b619b8
daedan <kidpia@hotmail.co.kr>
2021-12-13 15:14:20 +0900

    대시보드14 온도모니터링 출력 수정

--- src/main/java/daedan/mes/dash/service/Dash10Service.java
+++ src/main/java/daedan/mes/dash/service/Dash10Service.java
@@ -4,6 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 public interface Dash10Service {
-    List<Map<String, Object>> getTmpr10List(Map<String, Object> paraMap);
+    Map<String, Object> getTmpr10List(Map<String, Object> paraMap);
     Map<String, Object>  getFinalMetalDetect(Map<String, Object> paraMap);
 }
